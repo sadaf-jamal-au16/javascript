87cbc4a239a5373196dbf3b1ff0b364d61f69fe9
Sadaf <jamalsadaf3@gmail.com>
2020-12-11 21:25:10 +0530

    Committed

---
+++ Advance_JavaScript-Assignment/Assignment-2nd/Q5.js
@@ -0,0 +1,11 @@
+// Prints a simple multidimensional array in JavaScript 
+ 
+var arr1 = ["ABC", 24, 18000]; 
+var arr2 = ["EFG", 30, 30000]; 
+var arr3 = ["IJK", 28, 41000]; 
+var arr4 = ["EFG", 31, 28000]; 
+var arr5 = ["EFG", 29, 35000]; 
+var salary = [arr1, arr2, arr3, arr4, arr5]; 
+for(var i = 0; i < salary.length; i++) { 
+	document.write(salary[i] + "<br>"); 
+} 
